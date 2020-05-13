4107d02806f1f02ac3fcad5f92804b48535f7828
Gabriel Debona <gabriel.debona@gmail.com>
2020-05-13 14:05:42 -0300

    Add deploy script

---
+++ components/Menu/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Content = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  padding: 20px 30px;
+  background: ${props => props.intern ? 'url(/fundo-blur.png)' : 'none'};
+  height: ${props => props.intern ? '160px' : '100%'};
+  background-size: cover;
+  background-position: center bottom;
+
+  & .icon {
+    margin: 0 20px;
+    transition: opacity .5s ease;
+    cursor: pointer;
+    
+    &:hover {
+      opacity: 0.7;
+    }
+
+    &:last-child {
+      margin-right: 0;
+    }
+
+    @media (min-width: 768px) {
+      margin: 0 20px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    padding: 30px 40px;
+    height: ${props => props.intern ? '400px' : '100%'};
+    background-position: center top;
+  }
+`
